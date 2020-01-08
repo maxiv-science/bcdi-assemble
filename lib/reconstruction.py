Fixed the probability calculation so that the Poisson noise model doesn't explode - reconstruction runs

lib/reconstruction.py
@@ -10,24 +10,28 @@ def first_axis_com(a):
 def M(W, priors, data, prior_decay=1.0):
     Nj = W.shape[0]
     Nk = data.shape[0]
-    Rjk = np.empty((Nj, Nk), dtype=np.complex128)
+    logRjk = np.empty((Nj, Nk), dtype=np.complex128)
     wjk = np.empty((Nj, Nk), dtype=float)
 
     # first, calculate the probabilities Pjk based on the current model
     W[:] = W / np.mean(np.abs(W)) * np.mean(data)
     for j in range(Nj):
         for k in range(Nk):
-            Rjk[j, k] = np.exp(np.sum(data[k] * np.log10(W[j]+1e-6) - W[j]))
+            logRjk[j, k] = np.sum(data[k] * np.log(W[j]) - W[j])
             # prior weight for each observation biased towards where it was placed last time:
             wjk[j, k] = np.exp(-np.abs(j-priors[k])/prior_decay)
-    Pjk = wjk*Rjk / np.sum(wjk*Rjk, axis=0)
+    logPjk = np.log(wjk) + logRjk
+    # pragmatic pre-normalization to avoid overflow
+    logPjk -= np.max(logPjk, axis=0)
+    Pjk = np.real(np.exp(logPjk))
+    Pjk /= np.sum(Pjk, axis=0)
     
     # then carry out the likelihood maximization (M) step
     for j in range(Nj):
         W[j][:] = 0.0
         for k in range(Nk):
             W[j][:] = W[j] + Pjk[j][k] * data[k]
-        W[j][:] = W[j] / np.sum(Pjk[j, :])
+        W[j][:] = W[j] / (np.sum(Pjk[j, :]) + 1e-20)
 
     # then update the prior weights
     priors = first_axis_com(np.abs(Pjk))

single_reconstruction.py
@@ -10,7 +10,7 @@ N = 15
 
 ### load and plot generated data
 filename = 'data/test_data.npz'
-data = np.load(filename)['frames'] * 1e-6
+data = np.load(filename)['frames']
 o = np.load(filename)['offsets']
 ax = plt.gca()
 ax.plot(o, 'r', label='theta')
@@ -29,6 +29,7 @@ ax[1].set_title('a central frame')
 
 ### build and plot the initial model from the autocorrelation envelope
 W, envelope = generate_initial(N, data.shape[-1], support=.5)
+W += 1e-6
 
 ### iteration time!
 priors = np.linspace(0, N, len(data))