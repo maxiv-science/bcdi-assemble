5fa11a44a84af0bc54671783e503729fb14c178f
Alexander Bjorling <alexander.bjorling@maxiv.lu.se>
2020-05-21 21:48:25 +0200

    modified the body_strain example a bit

--- examples/body_strain/prepare_pynx.py
+++ examples/body_strain/prepare_pynx.py
@@ -6,8 +6,14 @@ import os
 assfiles = [f for f in os.listdir() if 'assembled_' in f and f.endswith('.npz')]
 
 for filename in assfiles:
-	data = np.load(filename)['W_ortho']
+	data = np.load(filename)['W'][5:-5]
 	strain = filename.split('.npz')[0].split('assembled_')[1]
+
+	# pad to equal side, it gets weird otherwise
+	pad = data.shape[-1] - data.shape[0]
+	before = (pad + 1) // 2
+	after = pad // 2
+	data = np.pad(data, ((before, after), (0, 0), (0, 0)), mode='constant', constant_values=0)
 
 	# we can at least roll the peak to the center
 	com = np.sum(np.indices(data.shape) * data, axis=(1,2,3)) / np.sum(data)
